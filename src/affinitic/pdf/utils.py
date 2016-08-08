b7110a1eb64b3d0ba70815543e90a9619d732125
FBruynbroeck <francois@affinitic.be>
2016-08-08 10:27:53 +0200

    Adding functions to compare two pdf refs #8092

---
+++ src/affinitic/pdf/utils.py
@@ -0,0 +1,44 @@
+# encoding: utf-8
+from cStringIO import StringIO
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+
+import difflib
+
+
+def diff_pdfs(expected, actual):
+    exp = convert_pdf_to_txt(expected)
+    act = convert_pdf_to_txt(actual)
+    return unidiff_output(exp, act)
+
+
+def convert_pdf_to_txt(path):
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    fp = file(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos = set()
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages,
+                                  password=password, caching=caching,
+                                  check_extractable=True):
+        interpreter.process_page(page)
+    text = retstr.getvalue()
+    fp.close()
+    device.close()
+    retstr.close()
+    return text
+
+
+def unidiff_output(expected, actual):
+    expected = expected.splitlines(1)
+    actual = actual.splitlines(1)
+    diff = difflib.unified_diff(expected, actual)
+    return ''.join(diff)
